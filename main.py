73269d643b2a9e4cbb6d863675a537f9ae26fa99
arhamahmed <arham.b.ahmed@gmail.com>
2022-04-20 22:23:19 -0700

    Refactor / cleanup main

--- main.py
+++ main.py
@@ -12,156 +12,70 @@
 #   - 4-layer lstm
 #   - fully-connected (4 layer?) lstm
 import numpy as np
-import pandas as pd
 import os
 # note: need .python. namespace for (newer?) versions of tensorflow
 from tensorflow.python.keras.layers import Embedding, Dense, CuDNNLSTM
 from tensorflow.python.keras.initializers import Constant
-from tensorflow.python.keras.models import Sequential, Model, Input
+from tensorflow.python.keras.models import Sequential
 
-# might need to use this for the FC-LSTM structure
-from tensorflow_addons.rnn.peephole_lstm_cell import PeepholeLSTMCell
-from tensorflow_addons.layers import CRF
-
-from keras_preprocessing.text import Tokenizer
-from keras_preprocessing.sequence import pad_sequences
-
-# from tensorflow.keras.utils.np_utils import to_categorical
 from sklearn.metrics import multilabel_confusion_matrix
 from sklearn.model_selection import train_test_split
 
-from utils import get_tag_indices, get_simple_batch, convert_to_tuples, get_stats
+from utils import get_embedding_matrix, get_formatted_data, get_stats, get_word_sequences, get_word_to_index, load_dataset, load_glove
 
-print("loading glove")
-glove_embeddings = {}
-f = open(os.path.join('./data/glove.6B', 'glove.6B.300d.txt'), encoding="utf-8")
-for line in f:
-    parts = line.split()
-    word = parts[0]
-    # this is the actual vector form of the word
-    values = np.asarray(parts[1:], dtype='float32')
-    glove_embeddings[word] = values
-f.close()
-print('Loaded %s glove word vectors.' % len(glove_embeddings))
+print("Loading pretrained GloVe embeddings")
+glove_embeddings = load_glove(os.path.join('./data/glove.6B', 'glove.6B.300d.txt'))
 
 tag2index = { "B-problem": 0, "I-problem": 1, "B-treatment": 2, "I-treatment": 3, "B-test": 4, "I-test": 5, "O": 6, "<unw>": 7, "<padding>": 8 }
 
-print("loading training data")
-training_data = np.loadtxt('./processed/train.txt', comments=None, dtype=str)
-training_data_df = pd.DataFrame({'word': training_data[:, 0], 'line_num': training_data[:, 1], 'tag': training_data[:, 2]})
-training_data_df["line_num"] = training_data_df["line_num"].astype(int)
+print("Loading datasets")
+training_data_df = load_dataset('./processed/train.txt')
+test_data_df = load_dataset('./processed/test.txt')
 
-print("loading test data")
-test_data = np.loadtxt('./processed/test.txt', comments=None, dtype=str)
-test_data_df = pd.DataFrame({'word': test_data[:, 0], 'line_num': test_data[:, 1], 'tag': test_data[:, 2]})
-test_data_df["line_num"] = test_data_df["line_num"].astype(int)
+word2index = get_word_to_index(training_data_df, test_data_df)
 
-train_word_tokenizer = Tokenizer(num_words=len(training_data_df), split=' ', oov_token='<unw>', filters=' ')
-train_word_tokenizer.fit_on_texts(training_data_df['word'].values)
-
-test_word_tokenizer = Tokenizer(num_words=len(test_data_df), split=' ', oov_token='<unw>', filters=' ')
-test_word_tokenizer.fit_on_texts(test_data_df['word'].values)
-
-word2index = test_word_tokenizer.word_index | train_word_tokenizer.word_index
-
-all_lines = training_data_df.groupby('line_num').apply(convert_to_tuples).tolist()
-X = [[word2index[word_tag_pair[0].lower()] for word_tag_pair in line] for line in all_lines]
-Y = [[tag2index[word_tag_pair[1]] for word_tag_pair in line] for line in all_lines]
-
-# remove the added docstart/emptyline tags from the words and tags
-X.pop(0)
-Y.pop(0)
+X, Y = get_word_sequences(training_data_df, word2index, tag2index)
 
 longest_line = len(max(X, key=len))
-print("longest line/sentence: ", longest_line)
 word2index["<padding>"] = len(word2index)
-X = pad_sequences(X, maxlen=longest_line, padding="post", value=word2index["<padding>"])
-Y = pad_sequences(Y, maxlen=longest_line, padding="post", value=tag2index["<padding>"])
-Y = [ np.eye(len(tag2index))[line] for line in Y]
+X, Y = get_formatted_data(X, Y, word2index, tag2index, longest_line)
+
 X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.15, random_state=123)
-X_train = np.array(X_train)
-X_val = np.array(X_val)
-Y_train = np.array(Y_train)
-Y_val = np.array(Y_val)
-
-# TODO: fix label processing too? double check at least
-
-# TODO
-# tokenize to properly parsed (case, punctuation) w2i
-# parse data into sentences using rowIndex -> t = [[tokens of s1], [tokens of s2], ...] = t1
-# parse tags data into sentences using rowIndex -> t = [[tags of s1], [tags of s2], ...] = t2
-# pad each of the above such that all t1 entries of same size and t2 entries of same size. use special pad char
-# update tag and word indices to include padding character
-# convert t2 from array of sentences containing arrays of tags for each word to:
-#   array of sentences with arrays of tags for each word in 1-hot vector form
-# the max padded length = embedding length
-# create train and validation split using train_test_split
-# pass train split to fit, and validation split to test
-# pass test data to predict()
 
 embedding_dim = 300
 vocab_size = len(word2index)
-word_embedding_matrix = np.zeros((vocab_size + 1, embedding_dim))
-
-hit = 0
-miss = 0
-
-print("mapping now")
-for word, index in word2index.items():
-    embedding_vector = glove_embeddings.get(word)
-    if embedding_vector is not None:
-        hit += 1
-        word_embedding_matrix[index] = embedding_vector
-    else:
-        miss += 1
-        # print("missed: ", word)
-        word_embedding_matrix[index] = np.random.randn(embedding_dim)
-
-# TODO: paper had ~13% miss rate, revisit data preprocessing/cleaning and use larger pretrained glove set; currently its >50% misses
-print('hit: ', hit, 'miss: ', miss)
-batch_size = 50
+word_embedding_matrix = get_embedding_matrix(embedding_dim, vocab_size, word2index, glove_embeddings)
 
 embedding_layer = Embedding(
     input_dim=(vocab_size + 1), 
     output_dim=embedding_dim, 
     input_length=longest_line, 
     embeddings_initializer=Constant(word_embedding_matrix), 
-    trainable=True # TODO this should be false once embedding/hitrate issue isfixed
+    trainable=True # TODO this should be false once embedding/hit-rate issue isfixed
 )
 
-print("building model")
+print("Building model")
 model = Sequential()
 model.add(embedding_layer)
 model.add(CuDNNLSTM(300, return_sequences=True))
 model.add(Dense(units=len(tag2index), activation='softmax'))
-
-print("running model")
 model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
 print(model.summary())
 
-model.fit(X_train, Y_train, batch_size=batch_size, epochs=20)
+print("Training model")
+model.fit(X_train, Y_train, batch_size=50, epochs=10)
 
-all_lines = test_data_df.groupby('line_num').apply(convert_to_tuples).tolist()
-X = [[word2index[word_tag_pair[0].lower()] for word_tag_pair in line] for line in all_lines]
-Y = [[tag2index[word_tag_pair[1]] for word_tag_pair in line] for line in all_lines]
-
-X.pop(0)
-Y.pop(0)
-
-X = pad_sequences(X, maxlen=longest_line, padding="post", value=word2index["<padding>"])
-Y = pad_sequences(Y, maxlen=longest_line, padding="post", value=tag2index["<padding>"])
-Y = [ np.eye(len(tag2index))[line] for line in Y]
-X_test = np.array(X)
-Y_test = np.array(Y)
-
+X_test, Y_test = get_word_sequences(test_data_df, word2index, tag2index)
+X_test, Y_test = get_formatted_data(X_test, Y_test, word2index, tag2index, longest_line)
+# can use the below to verify on validation split
 # X_test = X_val
 # Y_test = Y_val
 
+print("Running predictions")
 y_hat = model.predict(X_test)
-# predicted_tags = get_tag_indices(y_hat)
 predicted_tags = np.argmax(y_hat, axis=2)
 y_true = np.argmax(Y_test, axis=2)
-print("overall accuracy: ", (predicted_tags == y_true).mean())
+print("Overall model accuracy: ", (predicted_tags == y_true).mean())
 print('---------------------')
 
 confusion_matrix = multilabel_confusion_matrix(y_true.flatten(), predicted_tags.flatten())
--- utils.py
+++ utils.py
@@ -1,26 +1,79 @@
-import torch
 import numpy as np
+import pandas as pd
+from keras_preprocessing.text import Tokenizer
+from keras_preprocessing.sequence import pad_sequences
 
-def get_sequence(batch_of_tokens, mapping):
-    return torch.tensor([mapping[w] for w in batch_of_tokens], dtype=torch.long)
+def load_glove(path):
+    glove_embeddings = {}
+    f = open(path, encoding="utf-8")
+    for line in f:
+        parts = line.split()
+        word = parts[0]
+        # this is the actual vector form of the word
+        values = np.asarray(parts[1:], dtype='float32')
+        glove_embeddings[word] = values
+    f.close()
+    print('Loaded %s glove word vectors.' % len(glove_embeddings))
+    return glove_embeddings
 
-def get_tag_indices(tag_scores):
-    length = tag_scores.shape[0]
-    tags = []
-    for i in range(length):
-        # getting index of highest (most likely tag) value
-        tags.append(int(np.argmax(tag_scores[i])))
-    return tags
+def load_dataset(path):
+    data = np.loadtxt(path, comments=None, dtype=str)
+    data_df = pd.DataFrame({'word': data[:, 0], 'line_num': data[:, 1], 'tag': data[:, 2]})
+    data_df["line_num"] = data_df["line_num"].astype(int)
+    return data_df
 
-def get_simple_batch(tokens, batch_size):
-    batches = []
-    for i in range(0, len(tokens), batch_size):
-        batches.append(tokens[i:i + batch_size])
-    return batches
+# tokenize the data to generate a word2index dictionary 
+def get_word_to_index(train_data, test_data):
+    train_word_tokenizer = Tokenizer(num_words=len(train_data), split=' ', oov_token='<unw>', filters=' ')
+    train_word_tokenizer.fit_on_texts(train_data['word'].values)
+
+    test_word_tokenizer = Tokenizer(num_words=len(test_data), split=' ', oov_token='<unw>', filters=' ')
+    test_word_tokenizer.fit_on_texts(test_data['word'].values)
+
+    return test_word_tokenizer.word_index | train_word_tokenizer.word_index
+
+# using the mappings, convert sequences of words to sequences of indices
+def get_word_sequences(data, word2index, tag2index):
+    all_lines = data.groupby('line_num').apply(convert_to_tuples).tolist()
+    X = [[word2index[word_tag_pair[0].lower()] for word_tag_pair in line] for line in all_lines]
+    Y = [[tag2index[word_tag_pair[1]] for word_tag_pair in line] for line in all_lines]
+
+    # remove the added docstart/emptyline tags from the words and tags
+    X.pop(0)
+    Y.pop(0)
+    return X, Y
+
+# ensure all sequences are of equal length, and that y is in the form of an indicator variable
+def get_formatted_data(X, Y, word2index, tag2index, maxlength):
+    X = pad_sequences(X, maxlen=maxlength, padding="post", value=word2index["<padding>"])
+    Y = pad_sequences(Y, maxlen=maxlength, padding="post", value=tag2index["<padding>"])
+    Y = [ np.eye(len(tag2index))[line] for line in Y]
+    return np.array(X), np.array(Y)
 
 def convert_to_tuples(data):
     iterator = zip(data["word"].values.tolist(), data["tag"].values.tolist())
     return [(word, tag) for word, tag in iterator]
+
+def get_embedding_matrix(embedding_dim, vocab_size, word2index, embeddings):
+    embedding_dim = 300
+    word_embedding_matrix = np.zeros((vocab_size + 1, embedding_dim))
+
+    hit = 0
+    miss = 0
+
+    for word, index in word2index.items():
+        embedding_vector = embeddings.get(word)
+        if embedding_vector is not None:
+            hit += 1
+            word_embedding_matrix[index] = embedding_vector
+        else:
+            miss += 1
+            # print("missed: ", word)
+            word_embedding_matrix[index] = np.random.randn(embedding_dim)
+    
+    # TODO: paper had ~13% miss rate, revisit data preprocessing/cleaning and use larger pretrained glove set; currently its >50% misses
+    print('hit: ', hit, 'miss: ', miss)
+    return word_embedding_matrix
 
 def get_stats(conf_matrix, tag_index, tag_name):
     true_neg, false_pos, false_neg, true_pos = conf_matrix[tag_index].ravel()
